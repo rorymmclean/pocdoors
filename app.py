fa5d3e83a03a172001d15762ee55fa17c533d64b
rorymmclean <rory.mclean@aderas.com>
2023-07-25 16:15:13 -0400

    New build

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 ### Imports
 import streamlit as st
-from langchain.agents import AgentType, initialize_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.tools import BaseTool, StructuredTool, Tool, tool
-from langchain.llms import OpenAI
-from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-from langchain import LLMMathChain
-from langchain import PromptTemplate, LLMChain
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.sql_database import SQLDatabase
 import os
-import openai
 import io
 import pymysql
-from contextlib import redirect_stdout
-from langchain.agents.agent_toolkits import create_python_agent
-from langchain.tools.python.tool import PythonREPLTool
-from langchain.python import PythonREPL
 from datetime import datetime
+import pandas as pd
+
 import langchain
 from langchain.cache import InMemoryCache
 langchain.llm_cache = InMemoryCache()
 from langchain.cache import SQLiteCache
 langchain.llm_cache = SQLiteCache(database_path="langchain.db")
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import Tool, tool
+from langchain.llms import OpenAI
+from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+from langchain import LLMMathChain
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+from langchain.vectorstores import Chroma
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.tools.python.tool import PythonREPLTool
+# from langchain.python import PythonREPL
+# from langchain.agents import AgentType, initialize_agent
+# from langchain.tools import BaseTool, StructuredTool
+# from langchain import PromptTemplate, LLMChain
+# import openai
+from contextlib import redirect_stdout
 
 ### CSS
 st.set_page_config(
@@ -66,15 +71,23 @@ def run_cyber(myquestion):
     st.chat_message("user").write(myquestion)
 
     ### Bring in my controlling documents and the additonal template
-    with open('content/cybersecurity.txt') as f:
-        tasks = f.readlines()
-    mytasks = str(tasks)    
-    template=f"""You are a cybersecurity expert. 
-    Provide a detailed answer to the "QUESTION" below. 
-    Use the "TEXT" below to help develop to steps necessary to answer this question.
-    (When running a step the Thought/Action/Action Input/Observation can be repeated just 1 time.)
-    If you can't answer the question return the answer: "Sorry, but I can't help you with that task."
+    relevancy_cutoff = .5
+    myprompts = pd.read_csv('content/myprompts.csv')
 
+    db3 = Chroma(persist_directory="./chromadb", 
+                collection_name="runbook", 
+                embedding_function=OpenAIEmbeddings())
+
+    docs = db3.similarity_search_with_relevance_scores(myquestion, k=8)
+    mytasks = ""
+    for x,v in docs:
+        if mytasks == "":
+            mytasks = str(x)[14:-13] + "\n\n"
+            continue
+        if v < relevancy_cutoff:
+            mytasks = mytasks + str(x)[14:-13] + "\n\n"
+
+    template=f"""{myprompts[myprompts['name']=='Template']['prompt'].values[0]}
     TEXT:
     {mytasks}
 
@@ -86,7 +99,7 @@ def run_cyber(myquestion):
     ### Build an agent that can be used to run SQL queries against the database
     llm4 = ChatOpenAI(model="gpt-4", temperature=0, verbose=False)
     llm = ChatOpenAI(model=llm_model, temperature=0, verbose=False)
-    mydb = SQLDatabase.from_uri("mariadb+pymysql://streamlit:streamlitpass@pocmeetup_db_1/streamlit?charset=utf8mb4")
+    mydb = SQLDatabase.from_uri("mariadb+pymysql://streamlit:streamlitpass@localhost/streamlit?charset=utf8mb4")
     toolkit = SQLDatabaseToolkit(db=mydb, llm=llm)
 
     sql_agent = create_sql_agent(
@@ -103,23 +116,12 @@ def run_cyber(myquestion):
         Tool(
             name="Calculator",
             func=llm_math_chain.run,
-            description="useful for when you need to answer questions about math"
+            description=myprompts[myprompts['name']=='Calculator']['prompt'].values[0]
         ),
         Tool(
             name="SQL",
             func=sql_agent.run,
-            description="""This tool queries a SQLite database. It is useful for when you need to answer questions 
-            by running SQLite queries. Always indicate if your response is a "thought" or a "final answer". 
-            The following table information is provided to help you write your sql statement. 
-            Use the emails table to determine email information.
-            If you are looking for employee information within the emails table use the from_name or to_name column.
-            Use the apache_logs table to determine access information.
-            If you are looking for employee information within the apache_logs table use the "user" column.
-            If you are looking for date or time information within the apache_logs table use the dt column. 
-            
-            apache_logs: (ID, ip, user, dt, tz, vrb, uri, resp, byt, referer, useragent)
-            emails: (send_date, from_name, to_name, subject, body, attachment_type, filesize, sentiment)
-            """
+            description=myprompts[myprompts['name']=='SQL']['prompt'].values[0]
         )
     ]
 
@@ -155,7 +157,7 @@ def run_cyber(myquestion):
             st.write(s)
 
 with st.sidebar: 
-    mysidebar = st.selectbox('Select GamePlan', ['Cybersecurity', 'Data Science'])
+    mysidebar = st.selectbox('Select GamePlan', ['Cybersecurity'])
     if mysidebar == 'Cybersecurity':
         show_detail = st.checkbox('Show Details')
         llm_model = st.selectbox('Select Model', ['gpt-4', 'gpt-3.5-turbo'])
@@ -165,15 +167,6 @@ with st.sidebar:
         offhours = st.button('Offhour Access')
         st.markdown("---")
         tz = st.container()
-    if mysidebar == 'Data Science':
-        st.markdown("---")
-        st.markdown("### Planner Chain:")
-        st.markdown("&nbsp;&nbsp;&nbsp; Internet Search")
-        st.markdown("### Executor Chain:")
-        st.markdown("&nbsp;&nbsp;&nbsp; Python")
-        st.markdown("&nbsp;&nbsp;&nbsp; SQL")
-        st.markdown("&nbsp;&nbsp;&nbsp; Pandas")
-        st.markdown("&nbsp;&nbsp;&nbsp; Memory")
 
 if mysidebar == 'Cybersecurity':
     with st.expander("**:blue[Cybersecurity Overvew]**"):
@@ -210,30 +203,3 @@ if mysidebar == 'Cybersecurity':
         run_cyber("Using the \"apache_logs\" table, create and ordered list of the top five users based upon their total accesses that occur between the hours of 8pm and 6am. Sort the list in descending order by \"total accesses\". ") 
         tz.write("End: "+str(datetime.now())[10:])
         tz.write("Duration: "+str(datetime.now() - start))
-
-
-if mysidebar == 'Data Science':
-
-    agent_executor = create_python_agent(
-        llm=OpenAI(temperature=0, max_tokens=1000),
-        tool=PythonREPLTool(),
-        verbose=True
-    )
-
-    st.markdown("### **:blue[Overview:]** ")
-    st.markdown("**:blue[This gameplan is used to answer questions where dynamic Python is required. It also has the ability to query databases to extract the data necessary for the analysis.]**")
-    st.markdown("**:blue[It can write regular python, SciKit, Pytorch...for example, ask *'write a single neuron neural network in PyTorch. Take synthetic data for y=2x. Train for 1000 epochs and print every 100 epochs. Return prediction for x = 5'*]**")
-    myquestion = st.text_input('Question', placeholder='Ask a question?', value="What is the 10th fibonacci number?")
-
-    if myquestion:
-        with st.spinner('Running LangChain...Please Wait...'):
-            myresults = agent_executor.run(myquestion)
-            st.write(myresults)
-
-    st.markdown('---')
-    with st.expander("See explanation"):
-        st.markdown("**:blue[A Planner/Executor SuperChain is used in this model. In the Planner phase, the LLM searches the internet to determine how to solve the problem. It then developes a set of steps for the Executor phase.]**")
-        st.markdown("**:blue[In the Executor phase the LLM instructs LangChain how to perform each step. The chain has access to the data lakehouse, dynamic Python, a Pandas tool, and a memory tool so data can be moved between tools.]**")
-
-        col1, col2, col3 = st.columns([15, 70, 15])
-        col2.image('python.jpg',caption='LangChain Structure')
